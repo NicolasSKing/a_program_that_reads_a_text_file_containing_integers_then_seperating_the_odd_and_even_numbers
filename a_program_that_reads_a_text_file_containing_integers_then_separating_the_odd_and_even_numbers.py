c98b8c9b07cd9e8bb97803f0eaf33c79d464446d
NicolasSKing <nocoben10@gmail.com>
2026-04-23 14:45:50 +0800

    here_i_first_made_the_class_then_i_made_the_first_def_function_containing_the_init_code_and_then_i_added_self.numbers_all_the_numbers_will_first_go_here_the_self.odd_numbers_here_i_will_append_all_the_odd_numbers_same_as_the_self.even_numbers_here_i_will_append_all_the_even_numbers

---
+++ a_program_that_reads_a_text_file_containing_integers_then_separating_the_odd_and_even_numbers.py
@@ -0,0 +1,7 @@
+class NumberProcessor:
+    def __init__(self, filename):
+        self.filename = filename
+        self.numbers = []
+        self.odd_numbers = []
+        self.even_numbers = []
+    
