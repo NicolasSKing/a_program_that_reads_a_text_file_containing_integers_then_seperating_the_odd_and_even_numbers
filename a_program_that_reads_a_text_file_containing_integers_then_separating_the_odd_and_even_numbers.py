78a62331926404435dd78bdc91a36486562a8201
NicolasSKing <nocoben10@gmail.com>
2026-04-23 14:53:04 +0800

    i_made_the_second_function_name_read_numbers_the_purpose_of_this_function_is_to_read_all_the_data_from_the_filename_then_i_made_a_variable_called_data_here_it_will_store_the_data_that_it_reads

--- a_program_that_reads_a_text_file_containing_integers_then_separating_the_odd_and_even_numbers.py
+++ a_program_that_reads_a_text_file_containing_integers_then_separating_the_odd_and_even_numbers.py
@@ -4,4 +4,8 @@ class NumberProcessor:
         self.numbers = []
         self.odd_numbers = []
         self.even_numbers = []
-    
+
+    def read_numbers(self):
+        file = open(self.filename, "r")
+        data = file.read()
+        file.close()
